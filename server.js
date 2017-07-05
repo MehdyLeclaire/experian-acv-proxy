80ceb4d3164f863cb7a13be437f285ffaec1b0d3
Mehdy Leclaire <mehdy.leclaire@nuuvo.mobi>
2017-07-05 13:48:59 +0200

    tabs to spaces (to trigger docker hub automated build)

--- server.js
+++ server.js
@@ -27,27 +27,27 @@ app.use(expressWinston.errorLogger({
 app.listen(8080);
 
 app.get('/', function (req, res) {
-	var parsedUrl = url.parse(req.url,true);
-	var vin = parsedUrl.query.vin;
+  var parsedUrl = url.parse(req.url,true);
+  var vin = parsedUrl.query.vin;
     var cid = parsedUrl.query.cid;
     var sid = parsedUrl.query.sid;
     var password = parsedUrl.query.password;
     if (vin) {
-		var level = parsedUrl.query.level ? parsedUrl.query.level : 'indicators';
-		request.post(
-			'http://www.experian.com/ais/servlets/VHRXML', { 
-				form: {
-					'vinlist': vin,
-					'id': cid,
-					'password': password,
-					'level': level,
+    var level = parsedUrl.query.level ? parsedUrl.query.level : 'indicators';
+    request.post(
+      'http://www.experian.com/ais/servlets/VHRXML', {
+        form: {
+          'vinlist': vin,
+          'id': cid,
+          'password': password,
+          'level': level,
                     'sid': sid
-				}
-			}
-		).pipe(res);
-	} else {
-		res.sendStatus(200);
-	}
+        }
+      }
+    ).pipe(res);
+    } else {
+    res.sendStatus(200);
+    }
 });
 
 console.log("Server is listening to port 8080");
